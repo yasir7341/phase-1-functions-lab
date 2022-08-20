f8018cc28411502f90644aca017cd2fa93bef128
yasir7341 <yasirabdulrahman75@gmail.com>
2022-08-20 12:09:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+function(fileBiengTest){
+    fileBiengTest(index.js)
+}
+distanceFromHqInBlocks((pickUpPoint)-(HqInLocation)){
+    pickUpPoint(60)
+    HqInLocation(43)
+}
+function distanceTravelledInFeet(pickUpPoint, HqInLocation) {
+    it('returns a distance in blocks', function(60-43)
+    distanceTravelledInFeet(23)
+  }
+  expect(distanceFromHqInBlocks(43)).to.equal(1)
